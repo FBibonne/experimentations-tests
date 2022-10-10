bb454582dbdce791b349132f7c90da0bb85a0abf
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2022-10-10 15:23:20 +0200

    remove hard coded boring insee proxy : fill a prop if you want to use a proxy

--- src/main/java/fr/insee/test/App.java
+++ src/main/java/fr/insee/test/App.java
@@ -1,5 +1,8 @@
 package fr.insee.test;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -19,6 +22,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Optional;
 
 /**
  * Hello world!
@@ -28,6 +32,13 @@ import java.net.http.HttpResponse;
 public class App
 {
 
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    @Value("${fr.insee.test.proxy.host:#{null}}")
+    private Optional<String> proxyHost;
+
+    @Value("${fr.insee.test.proxy.port:8080}")
+    private int proxyPort;
     private boolean runned=false;
     public static void main( String[] args )
     {
@@ -42,13 +53,16 @@ public class App
                     public Message<String> receive() {
                         if (!runned) {
                             runned = true;
-                            var httpClient = HttpClient.newBuilder().proxy(ProxySelector.of(new InetSocketAddress("proxy-rie.http.insee.fr", 8080))).build();
+                            var httpClientBuilder = HttpClient.newBuilder();
+                            proxyHost.ifPresent(s -> httpClientBuilder.proxy(ProxySelector.of(new InetSocketAddress(s, proxyPort))));
+                            var httpClient = httpClientBuilder.build();
                             var request = HttpRequest.newBuilder().uri(URI.create("https://api.chucknorris.io/jokes/random")).build();
                             try {
                                 var response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
                                 return MessageBuilder.withPayload(response.body()).build();
                             } catch (IOException | InterruptedException e) {
-                                return MessageBuilder.withPayload(e.getMessage()).build();
+                                logger.error("Error while fetching external WS", e);
+                                return MessageBuilder.withPayload(e.getClass()+" : "+e.getMessage()).build();
                             }
                         } else {
                             return null;
