55b636e019279dabcdfda4faea1b601ef2f3f4a9
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2022-12-04 08:44:54 +0100

    First test working

--- src/main/java/fr/insee/test/JsonStream.java
+++ src/main/java/fr/insee/test/JsonStream.java
@@ -1,9 +1,28 @@
 package fr.insee.test;
 
-import jakarta.json.stream.JsonParser;
+import jakarta.json.Json;
+import lombok.NonNull;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
 
 public class JsonStream {
 
-    private JsonParser jsonParser;
-
+    public static String process(@NonNull String emptyJson) {
+        var outputStream= new ByteArrayOutputStream();
+        try (var jsonParser = Json.createParser(
+                new ByteArrayInputStream(emptyJson.getBytes(StandardCharsets.UTF_8)));
+             var jsonGenerator = Json.createGenerator(outputStream)
+        ) {
+            while (jsonParser.hasNext()){
+                switch (jsonParser.next()){
+                    case START_OBJECT -> jsonGenerator.writeStartObject();
+                    case END_OBJECT -> jsonGenerator.writeEnd();
+                    default -> throw new IllegalStateException("Unexpected value: " + jsonParser.next());
+                }
+            }
+        }
+        return outputStream.toString(StandardCharsets.UTF_8);
+    }
 }
