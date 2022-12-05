c367f633046563c2a4e00271ca9b3ad2f07af7aa
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2022-12-05 22:11:17 +0100

    JsonValue avec entier

--- src/main/java/fr/insee/test/JsonStream.java
+++ src/main/java/fr/insee/test/JsonStream.java
@@ -5,24 +5,28 @@ import lombok.NonNull;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
 
 public class JsonStream {
 
-    public static String process(@NonNull String emptyJson) {
-        var outputStream= new ByteArrayOutputStream();
-        try (var jsonParser = Json.createParser(
-                new ByteArrayInputStream(emptyJson.getBytes(StandardCharsets.UTF_8)));
-             var jsonGenerator = Json.createGenerator(outputStream)
+    public static String process(@NonNull String json) {
+        var writer= new StringWriter();
+        try (var jsonParser = Json.createParser(new StringReader(json));
+             var jsonGenerator = Json.createGenerator(writer)
         ) {
+            String lastKey=null;
             while (jsonParser.hasNext()){
                 switch (jsonParser.next()){
                     case START_OBJECT -> jsonGenerator.writeStartObject();
                     case END_OBJECT -> jsonGenerator.writeEnd();
-                    default -> throw new IllegalStateException("Unexpected value: " + jsonParser.next());
+                    case KEY_NAME -> lastKey=jsonParser.getString();
+                    case VALUE_NUMBER -> jsonGenerator.write(lastKey, jsonParser.getLong());
+                    default -> throw new IllegalStateException("Unexpected value: " + jsonParser.currentEvent());
                 }
             }
         }
-        return outputStream.toString(StandardCharsets.UTF_8);
+        return writer.toString();
     }
 }
--- src/test/java/fr/insee/test/JsonStreamTest.java
+++ src/test/java/fr/insee/test/JsonStreamTest.java
@@ -1,16 +1,36 @@
 package fr.insee.test;
 
-import org.assertj.core.api.Assertions;
+import jakarta.json.Json;
+import jakarta.json.JsonReader;
+import jakarta.json.JsonStructure;
+import jakarta.json.JsonValue;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class JsonStreamTest {
+class JsonStreamTest {
 
-    @Test
-    public void streamEmpty(){
-        String emptyJson="{}";
-        assertThat(JsonStream.process(emptyJson)).isEqualTo(emptyJson);
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "{}",
+            "{\"a\":1}"
+    })
+    void testStreams(String jsonString){
+        // Given jsonString
+        //When
+        var jsonProcessed = JsonStream.process(jsonString);
+        // Then
+        assertThat(toJson(jsonProcessed)).isEqualTo(toJson(jsonString));
+    }
+
+    private JsonStructure toJson(String jsonString){
+        var reader=Json.createReader(new ByteArrayInputStream(jsonString.getBytes(StandardCharsets.UTF_8)));
+        return reader.read();
     }
 
 
